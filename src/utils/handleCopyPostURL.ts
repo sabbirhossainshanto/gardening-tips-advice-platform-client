80340fb6a0e767906122787ac51ae22f59969a9a
sabbirHossain <sabbirshnt@gmail.com>
2024-10-07 10:05:51 +0600

    updated

--- src/utils/handleCopyPostURL.ts
+++ src/utils/handleCopyPostURL.ts
@@ -2,7 +2,7 @@ import copy from "copy-to-clipboard";
 import { toast } from "sonner";
 
 const handleCopyPostURL = (id: string) => {
-  copy(`${window.location.origin}/${id}`);
+  copy(`${window.location.origin}/posts/${id}`);
   toast.success("Successfully copied the post url!");
 };
 
